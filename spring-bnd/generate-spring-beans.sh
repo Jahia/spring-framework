0227d53e86a5b061530f2858865a95ab0462cc3d
Cedric Mailleux <cmailleux@jahia.com>
2023-11-10 15:19:44 -0500

    QA-14375: Prepare release (https://confluence.jahia.com/display/PR/3rd-party+libraries+-+Ref+ISPOL08.A14024) (#2)

--- spring-bnd/generate-spring-beans.sh
+++ spring-bnd/generate-spring-beans.sh
@@ -1,15 +1,19 @@
-cp ~/.m2/repository/org/springframework/spring-beans/3.2.18.jahia1/spring-beans-3.2.18.jahia1.jar .
+#!/usr/bin/env bash
+source ../gradle.properties
+cp ~/.m2/repository/org/springframework/spring-beans/${version}/spring-beans-${version}.jar .
 
-cp ~/.m2/repository/org/springframework/spring-beans/3.2.18.jahia1/spring-beans-3.2.18.jahia1.pom .
+cp ~/.m2/repository/org/springframework/spring-beans/${version}/spring-beans-${version}.pom .
 
-sed -i .bak s/3.2.18.jahia1/3.2.18.jahia1_OSGI/g *.pom
+sed -i .bak s/${version}/${version}_OSGI/g *.pom
 
-bnd *.bnd
+bnd spring-beans.bnd
 
-jar xf spring-beans-3.2.18.jahia1.jar META-INF ; rm META-INF/MANIFEST.MF ; jar uf spring-beans-3.2.18.jahia1_OSGI.jar META-INF/* ; rm -Rf META-INF
+jar xf spring-beans-${version}.jar META-INF ; rm META-INF/MANIFEST.MF ; jar uf spring-beans-${version}_OSGI.jar META-INF/* ; rm -Rf META-INF
 
-mvn install:install-file -Dfile=spring-beans-3.2.18.jahia1_OSGI.jar -DpomFile=spring-beans-3.2.18.jahia1.pom
+mvn install:install-file -Dfile=spring-beans-${version}_OSGI.jar -DpomFile=spring-beans-${version}.pom
 
-# mvn deploy:deploy-file -DrepositoryId=jahia-releases -Durl=https://devtools.jahia.com/nexus/content/repositories/thirdparty-releases/ -Dfile=spring-beans-3.2.18.jahia1_OSGI.jar -DpomFile=spring-beans-3.2.18.jahia1.pom
+
+# Uncomment the following line for release
+#mvn deploy:deploy-file -DrepositoryId=jahia-releases -Durl=https://devtools.jahia.com/nexus/content/repositories/thirdparty-releases/ -Dfile=spring-beans-${version}_OSGI.jar -DpomFile=spring-beans-${version}.pom
 
 rm *.jar *.pom *.bak
