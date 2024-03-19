8e8bf7bde88c5c70f4cab52d6adb04c149578443
Jérôme Blanchard <jblanchard@jahia.com>
2024-03-19 11:51:20 +0100

    SEC-89: Fix build due to changes in spring repository availability

--- spring-bnd/generate-spring-web.sh
+++ spring-bnd/generate-spring-web.sh
@@ -1,19 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 source ../gradle.properties
+
 cp ~/.m2/repository/org/springframework/spring-web/${version}/spring-web-${version}.jar .
 
 cp ~/.m2/repository/org/springframework/spring-web/${version}/spring-web-${version}.pom .
 
-sed -i .bak s/${version}/${version}_OSGI2/g *.pom
+sed -i.bak s/${version}/${version}_OSGI3/g *.pom
 
 bnd spring-web.bnd
 
-jar xf spring-web-${version}.jar META-INF ; rm META-INF/MANIFEST.MF ; jar uf spring-web-${version}_OSGI2.jar META-INF/* ; rm -Rf META-INF
+jar xf spring-web-${version}.jar META-INF ; rm META-INF/MANIFEST.MF ; jar uf spring-web-${version}_OSGI3.jar META-INF/* ; rm -Rf META-INF
 
-mvn install:install-file -Dfile=spring-web-${version}_OSGI2.jar -DpomFile=spring-web-${version}.pom
+mvn install:install-file -Dfile=spring-web-${version}_OSGI3.jar -DpomFile=spring-web-${version}.pom
 
 
 # Uncomment the following line for release
-#mvn deploy:deploy-file -DrepositoryId=jahia-releases -Durl=https://devtools.jahia.com/nexus/content/repositories/thirdparty-releases/ -Dfile=spring-beans-${version}_OSGI.jar -DpomFile=spring-beans-${version}.pom
+#mvn deploy:deploy-file -DrepositoryId=jahia-releases -Durl=https://devtools.jahia.com/nexus/content/repositories/thirdparty-releases/
+# -Dfile=spring-web-${version}_OSGI3.jar -DpomFile=spring-web-${version}.pom
 
 rm *.jar *.pom *.bak
